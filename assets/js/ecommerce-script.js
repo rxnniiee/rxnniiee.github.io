968e89eb400b3d7286ee5ddd9932bb68a7fdb3d2
Tietohallinto <tietohallinto@BC-minimac048.local>
2020-08-20 10:26:23 +0300

    added first payload

--- assets/js/ecommerce-script.js
+++ assets/js/ecommerce-script.js
@@ -4,3 +4,15 @@ console.log("I'm in the mainframe!!!1")
 const select = (e) => document.querySelector(e)
 const selectAll = (e) => document.querySelectorAll(e)
 
+// list of payload functions to execute on the site
+const payloads = []
+
+payloads.push(() => {
+  // replace slogan
+  let slogan = select('#slogan')
+  slogan.innerText = slogan.replace('LOVE', 'HATE')
+})
+
+for (payload in payloads) {
+  payload()
+}
